dc4ddf9bd6e6aa3cf1e59d62cda11b23abbc86eb
umesh <umesharma.us11@gmail.com>
2022-05-07 14:09:06 +0530

    final commit

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const colors = ["green",
     "#f15025"
 ];
 
-const btn =document.getElementById("btn")
-const color = document.querySelector(".color")
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
 
 btn.addEventListener("click", () => {
-    const randomNumber = getRandomNumber()
-    document.body.style.backgroundColor = colors[randomNumber]
-    color.textContent = colors[randomNumber] 
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber]; 
 })
 
 const getRandomNumber = () => {
-    return Math.floor(Math.random()*colors.length )
+    return Math.floor(Math.random() * colors.length);
 }
